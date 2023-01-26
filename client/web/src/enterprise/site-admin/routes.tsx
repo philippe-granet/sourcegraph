f48a77afdb93c2a8227e4fd545201224113870f8
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2023-01-26 22:30:35 +0100

    update

--- client/web/src/enterprise/site-admin/routes.tsx
+++ client/web/src/enterprise/site-admin/routes.tsx
@@ -2,7 +2,7 @@ import { Navigate, useLocation, useParams } from 'react-router-dom-v5-compat'
 
 import { lazyComponent } from '@sourcegraph/shared/src/util/lazyComponent'
 
-import { SiteAdminRolesPageProps } from './SiteAdminRolesPage'
+import { SiteAdminRolesPageProps } from '../rbac/SiteAdminRolesPage'
 import { siteAdminAreaRoutes } from '../../site-admin/routes'
 import { SiteAdminAreaRoute } from '../../site-admin/SiteAdminArea'
 import { BatchSpecsPageProps } from '../batches/BatchSpecsPage'
@@ -184,11 +184,10 @@ export const enterpriseSiteAdminAreaRoutes: readonly SiteAdminAreaRoute[] = (
         {
             path: '/roles',
             exact: true,
-            render: () => lazyComponent<SiteAdminRolesPageProps, 'SiteAdminRolesPage'>(
-                () => import('./SiteAdminRolesPage'),
+            render: lazyComponent<SiteAdminRolesPageProps, 'SiteAdminRolesPage'>(
+                () => import('../rbac/SiteAdminRolesPage'),
                 'SiteAdminRolesPage'
             ),
-            condition: (ctx) => ctx.isRbacEnabled,
         },
     ] as readonly (SiteAdminAreaRoute | undefined)[]
 ).filter(Boolean) as readonly SiteAdminAreaRoute[]
